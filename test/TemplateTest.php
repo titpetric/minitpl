13f549c05516a529c378fab5d14a9b07542dcae0
Tit Petric <tit.petric@monotek.net>
2013-05-08 15:38:07 +0200

    Variable compilation checks, to see if we don't fuck up syntax

--- test/TemplateTest.php
+++ test/TemplateTest.php
@@ -3,7 +3,7 @@
 class TemplateTest extends PHPUnit_Framework_TestCase
 {
 	/**
-	 * @dataProvider provider
+	 * @dataProvider compileProvider
 	 */
 	public function testCompile($template)
 	{
@@ -22,7 +22,7 @@ class TemplateTest extends PHPUnit_Framework_TestCase
 		$this->assertFileEquals($destination, $compiled);
 	}
 
-	public function provider()
+	public function compileProvider()
 	{
 		$tests = array();
 		$templates = glob("test/templates/*.tpl");
@@ -31,5 +31,29 @@ class TemplateTest extends PHPUnit_Framework_TestCase
 			$tests[] = array(basename($template));
 		}
 		return $tests;
+	}
+
+	/**
+	 * @dataProvider varsProvider
+	 */
+	public function testVars($expression, $expected)
+	{
+		$tpl = new Monotek\MiniTPL\Compiler;
+
+		$result = $tpl->_split_exp($expression);
+		$this->assertEquals($expected, $result);
+	}
+
+	public function varsProvider()
+	{
+		$vars = array();
+		$vars[] = array('news_section_news_list.tpl', 'news_section_news_list.tpl');
+		$vars[] = array('$var', "\$_v['var']");
+		$vars[] = array('$var.netko', "\$_v['var']['netko']");
+		$vars[] = array('$var . "netko"', "\$_v['var'] . \"netko\"");
+		$vars[] = array('$var1 . $var2', "\$_v['var1'] . \$_v['var2']");
+		$vars[] = array('$var1.$var2', "\$_v['var1'][\$_v['var2']]");
+		$vars[] = array('$items.0', "\$_v['items']['0']");
+		return $vars;
 	}
 }
